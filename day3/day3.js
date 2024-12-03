901945f493ad19ce1055a36542bca31a4c93329d
Noah van Boven <noah.van.boven@student.uva.nl>
2024-12-03 13:20:07 +0100

    Adding comments

--- day3/day3.js
+++ day3/day3.js
@@ -10,7 +10,6 @@ const part1 = (input) => {
     for (const match of matches) {
         const [_, a, b] = match;
 
-
         sum += Number(a) * Number(b);
     }
     
@@ -23,6 +22,7 @@ const part2 = (input) => {
     const matchesDo = [...input.matchAll(matchDo)].map(m => ({ index: m.index, type: 'do' }));
     const matchesDont = [...input.matchAll(matchDont)].map(m => ({ index: m.index, type: 'dont' }));
 
+    // Sort the matches by index so that we can iterate over them in order
     const matches = [...matchesDo, ...matchesDont].sort((a, b) => a.index - b.index);
 
     let startIndex = 0;
@@ -30,11 +30,15 @@ const part2 = (input) => {
     let prevType = 'do';
     let sum = 0;
     for (const todo of matches) {
+        // If our previous type is also a do, we don't need to update the 
+        // startIndex because it keeps counting towards our sum.
         if (todo.type === 'do' && prevType == 'dont') {
             startIndex = todo.index;
             prevType = 'do';
         }
 
+        // If our previous type is also a don't there's no need to update the 
+        // sum because the previous type was also a don't.
         if (todo.type === 'dont' && prevType == 'do') {
             endIndex = todo.index;
 
@@ -44,6 +48,7 @@ const part2 = (input) => {
         }
     }
 
+    // If the last type was a do, we need to add the last part to the sum
     if (prevType === 'do') {
         sum += part1(input.substring(startIndex));
     }
